e3161b96daca73846de6bfa1d549b17b030d9c27
Sebasch Amauri Santillan <sebaschamauri.09@gmail.com>
2025-11-30 23:27:16 +0800

    add home template

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,7 @@
-import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
-
+import './style.css'
 
 // main 
 document.querySelector('#app').innerHTML = `
